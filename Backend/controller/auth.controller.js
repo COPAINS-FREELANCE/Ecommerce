edc61003490960a718182333ac56dd908d0e6e98
JoshMusira <joshuamusira01@gmail.com>
2023-11-11 17:07:57 +0300

    feat:validate user

--- Backend/controller/auth.controller.js
+++ Backend/controller/auth.controller.js
@@ -4,7 +4,28 @@ const prisma = new PrismaClient()
 
 export const Register = async (req, res) => {
     try {
-        const data = req.body;
+        const { first_name, last_name, email, password } = req.body;
+
+        if (!first_name || !last_name || !email) {
+            return res.status(400).json({
+                message: "Please fill all fields"
+            })
+        }
+
+        const userExist = await prisma.user.findMany({
+            where: {
+                email
+            }
+        })
+
+        if (userExist) {
+            return res.status(400).json({
+                message: "User already exist"
+            })
+        }
+
+
+
         const newUser = await prisma.user.create({ data });
         res.json(newUser);
     } catch (error) {
